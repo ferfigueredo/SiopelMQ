87f251389a6c8455b5a392cd0859d452f4d34eaf
DESKTOP-NLS2JJV\Fernando Figueredo <ffigueredo@pkglobal.com>
2020-10-13 12:07:46 -0300

    commit inicial

---
+++ src/main/java/com/siopelMQ/SiopelMApplication.java
@@ -0,0 +1,13 @@
+package com.siopelMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiopelMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiopelMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siopelMQ/SiopelMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siopelMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiopelMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
